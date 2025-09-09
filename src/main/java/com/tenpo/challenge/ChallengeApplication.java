b43d13bb14254003d6d654043979e851c8bd8e89
Sebastian Albornoz <sebastian.albornozm@utem.cl>
2025-09-09 19:58:29 -0300

    feature: create sample endpoint to test spring app status

--- src/main/java/com/tenpo/challenge/ChallengeApplication.java
+++ src/main/java/com/tenpo/challenge/ChallengeApplication.java
@@ -2,9 +2,17 @@ package com.tenpo.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ChallengeApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "OK";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeApplication.class, args);
