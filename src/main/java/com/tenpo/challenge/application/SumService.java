27e29ca6017a4be4e5032c2e5dd5b78cf990f981
Sebastian Albornoz <sebastian.albornozm@utem.cl>
2025-09-15 10:23:00 -0300

    feat: implement base sum service

--- src/main/java/com/tenpo/challenge/application/SumService.java
+++ src/main/java/com/tenpo/challenge/application/SumService.java
@@ -1,5 +1,19 @@
 package com.tenpo.challenge.application;
 
+import org.springframework.stereotype.Service;
+
+import com.tenpo.challenge.domain.repository.CallHistoryRepository;
+
+@Service
 public class SumService {
-    
+    private final CallHistoryRepository callHistoryRepository;
+
+    public SumService(CallHistoryRepository callHistoryRepository) {
+        this.callHistoryRepository = callHistoryRepository;
+    }
+
+    public double sum(double a, double b){
+        double result = a + b;
+        return result;
+    }
 }
